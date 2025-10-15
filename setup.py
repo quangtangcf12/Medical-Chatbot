32618d0f1c72f8d9f7dab12355a16fdb999f8662
quangtangcf12 <ducgast123@gmail.com>
2025-10-16 01:46:14 +0700

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Tran Tri Duc",
+    author_email="22520276@gm.uit.edu.vn",
+    packages=find_packages(),
+    install_requires=[]
+)
